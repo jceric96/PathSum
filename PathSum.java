3425a8567e635c29a8a5e6c753374d2f99454059
jceric96 <74382751+jceric96@users.noreply.github.com>
2022-09-01 22:59:00 -0300

    path sum

---
+++ PathSum.java
@@ -0,0 +1,32 @@
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        targetSum -= root.val;
+        if (targetSum == 0 && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        PathSum tree = new PathSum();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.right = null;
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.left.left = null;
+        root.right.left.right = null;
+        root.right.right.left = null;
+        root.right.right.right = new TreeNode(4);
+
+        int targetSum = 22;
+        System.out.println(tree.hasPathSum(root, targetSum));
+    }
+}
